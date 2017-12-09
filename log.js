984e1f7fec713f85a07d07f65ec30586e5994664
Romuald Brillout <github.com@brillout.com>
2017-12-09 18:28:42 +0100

    add stringifcation of functions defined in objects

--- log.js
+++ log.js
@@ -2,6 +2,9 @@ log.logify_input = logify_input;
 
 module.exports = log;
 
+var project_name = 'Reassert';
+var stringification_name = '(Reassert-stringification)';
+
 function log() {
     for(var i in arguments) {
         var input = arguments[i];
@@ -11,20 +14,44 @@ function log() {
 }
 
 function logify_input(input) {
-    var PREFIX = '[object ';
+    if( ! input ) {
+        return input;
+    }
 
-    var str;
-    if( !input ) {
-        str = input;
-    } else {
-        str = input.toString();
-        if( str.slice(0, PREFIX.length) === PREFIX || input.constructor === Array ) {
-            try {
-                str = JSON.stringify(input, null, 2);
-            } catch(e) {
-                str += ' ['+e+']';
+    if( input.constructor === Array ) {
+        return stringify_object(input);
+    }
+
+    var PREFIX_UGLY = '[object ';
+    var input_str = input.toString();
+    if( input_str.slice(0, PREFIX_UGLY.length) === PREFIX_UGLY ) {
+        return stringify_object(input);
+    }
+
+    return input_str;
+}
+
+function stringify_object(obj) {
+    add_json_function_handlers(obj);
+    try {
+        return JSON.stringify(obj, null, 2);
+    } catch(e) {
+        return obj.toString()+'['+e+'][Error]'+stringification_name;
+    }
+}
+
+function add_json_function_handlers(obj) {
+    for(var key in obj) {
+        var el = obj[key];
+        if( el instanceof Function ) {
+            if( ! el.toJSON ) {
+                let str = '['+(el.name||'Anonymous function')+']'+stringification_name;
+                el.toJSON = function() {
+                    return str;
+                };
             }
+        } else if( el instanceof Object ) {
+            add_json_function_handlers(el);
         }
     }
-    return str;
 }
