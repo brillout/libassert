7047bc60c182461e13d6179ef8280aea7c9bbc96
Romuald Brillout <github.com@brillout.com>
2019-05-17 13:44:25 +0200

    bump

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const assert = require('./assert');
 const assert_internal = require('./internal');
 const assert_usage = require('./usage');
 const assert_warning = require('./warning');
+const log = require('./log');
 
 module.exports = assert;
 module.exports.internal = assert_internal;
 module.exports.usage = assert_usage;
 module.exports.warning = assert_warning;
+module.exports.log = log;
